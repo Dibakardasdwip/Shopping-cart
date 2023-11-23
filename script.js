45fc23cb2175fbe08332c530f562d2100bc89bf0
dibakardasdwip <121dibakar@gmail.com>
2023-11-23 19:34:08 +0600

    complete

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ class Product{
         fetch("product.json")
          .then(res=>res.json())
          .then(data=> {
-            let row = document.querySelector('.row');
+            let row = document.querySelector('.row-product');
             row.innerHTML = "";
-            data.forEach(item=>{
+            data.forEach((item,index)=>{
                 if(row !== ""){
                    row.innerHTML += `
                             <div class="col-4 py-4" style="width:280px">
@@ -20,7 +20,7 @@ class Product{
                                     <img src="${item.image}" id="product-img" class="image img-fluid " alt="">
                                   <p id="product-name">${item.name}</p >
                                   <h5 id="product-price">$${item.price}</h5>
-                                  <button class="btn btn-primary">Add to cart</button>
+                                  <button class="btn btn-primary btn-cart" id="add-btn-${index}">Add to cart</button>
                                 </div>
                               </div>
                         </div>
@@ -38,39 +38,44 @@ class Product{
 
 }
 
-
-
-
 let a = new Product();
-
-
-
 a.getData()
 
+// Add to cart Button
+document.addEventListener('click', (e)=>{
+    if(e.target.className === "btn btn-primary btn-cart"){
 
+        let parent = e.target.parentElement;
+        let cartImg = parent.firstElementChild;
+        let productName = cartImg.nextElementSibling;
+        let price = productName.nextElementSibling;
+        let sidebar = document.querySelector(".car-item");
 
-
-
-// let row = document.querySelector('.row');
-// items.forEach(item=>{
-//     row.innerHTML =""
-
-//     console.log(item);
+        sidebar.innerHTML += `
+        <div class="container-fluid d-flex cart-items text-center">
+        <img class="image img-fluid cart-img" ${cartImg.outerHTML.split(" ")[1]} alt="">
+        <h5>${productName.textContent}</h5>
+        <h5>${price.textContent}</h5>
+        <h5><a href="#">X</a></h5>
+      </div>`
+    }
     
-//     if(row !=""){
-//         row.innerHTML += `
-//         <div class="col-4" style="width:260px">
-//         <div class="card">
-//             <div class="card-body text-center">
-//                 <img src="${item.image}" id="product-img" class="image img-fluid " alt="">
-//               <h4 id="product-name">${item.name}</h4>
-//               <h5 id="product-price">$${item.price}</h5>
-//               <button class="btn btn-primary">Add to cart</button>
-//             </div>
-//           </div>
-//     </div>
-//         `
-//     }
+})
 
+
+
+// remove from cart
+let sidebar = document.querySelector('.sidebar');
+
+sidebar.addEventListener('click', e=>{
+    if(e.target.hasAttribute("href")){
+        let item = e.target.parentElement.parentElement.remove()
+    }
     
-// })
+})
+
+
+
+
+
+
